5b6f7d7b38d853668070624e0be0d6da90fa45c4
morrisonsswapnilkulkarni <swapnil.kulkarni@morrisonsplc.co.uk>
2019-11-20 07:22:02 +0000

    Initail commit - android update only

---
+++ src/App.js
@@ -0,0 +1,129 @@
+import React, { Component } from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <Text style={styles.instructions}>{instructions}</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+/* import {
+  StyleSheet,
+  Text,
+  View,
+  Platform,
+  TouchableHighlight,
+  Animated,
+  Easing,
+} from 'react-native';
+import logo from './logo.png';
+
+class App extends Component {
+  state = {
+    spinValue: new Animated.Value(0),
+  }
+
+  onClick = () => {
+    const wasRotated = this.state.spinValue._value === 1;
+    Animated.timing(
+      this.state.spinValue,
+      {
+        toValue: wasRotated ? 0 : 1,
+        duration: 250,
+        easing: Easing.linear
+      }
+    ).start()
+  }
+
+  render() {
+    const spin = this.state.spinValue.interpolate({
+      inputRange: [0, 1],
+      outputRange: ['0deg', '360deg']
+    });
+
+    return (
+      <View style={styles.container}>
+        <Animated.Image source={logo} style={[styles.logo, { transform: [{rotate: spin}] }]}/>
+        <Text style={styles.title}>Create React Native Web App</Text>
+        <Text>Open up src/App.js to start working on your app!</Text>
+        <Text>Changes you make will automatically reload.</Text>
+        {Platform.OS !== 'web' && <Text>Shake your phone to open the developer menu.</Text>}
+        <TouchableHighlight
+          onPress={this.onClick}
+          style={styles.button}
+          underlayColor={'#0A84D0'}
+        >
+          <Text style={styles.buttonText}>Rotate Logo</Text>
+        </TouchableHighlight>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logo: {
+    width: 300,
+    height: 300,
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+  button: {
+    borderRadius: 3,
+    padding: 20,
+    marginVertical: 10,
+    marginTop: 10,
+    backgroundColor: '#1B95E0',
+  },
+  buttonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+});
+
+let hotWrapper = () => () => App;
+if (Platform.OS === 'web') {
+  const { hot } = require('react-hot-loader');
+  hotWrapper = hot;
+}
+export default hotWrapper(module)(App); */
